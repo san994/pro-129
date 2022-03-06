26824131f55fdcece2f246747772d00febb62db2
san994 <75426313+san994@users.noreply.github.com>
2022-03-06 16:54:25 +0530

    Add files via upload

---
+++ merge2.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+columns = ["row_name","star_name","distance","mass","radius"]
+df1 = pd.read_csv("dwarf_star_data.csv",names=columns)
+df1 = df1.iloc[1: , :]
+
+columns = ["row_name","star_name","distance","mass","radius","luminosity"]
+df2 = pd.read_csv("star_data.csv",names=columns)
+df2 = df2.iloc[1: , :]
+
+
+final = pd.concat([df1, df2],axis=0)
+
+final.to_csv("final.csv",index=False)
